92475753ab5383a556a850019a533f262a30c7bb
Rick Joosten <halloikama_rick@hotmail.com>
2019-04-29 18:05:59 +0300

    Now uses FastText (takes about 2 mins to train)

--- roses/modules/alter_word_pairs.py
+++ roses/modules/alter_word_pairs.py
@@ -1,17 +1,25 @@
 from typing import List, Tuple
 import nltk
 import gensim
+import os
+from gensim.models import FastText
 from utils import read_json_file
+
+nltk.download('abc')
+nltk.download('brown')
+
+from nltk.corpus import abc
+from nltk.corpus import brown
+
 
 
 # we could possibly save the model to speed up the process
 def train_model():
-    nltk.download('abc')
-    from nltk.corpus import abc
 
     data = read_json_file("data/bible_kjv_wrangled.json")
     sentences = list(data.values())
-   
+    
+    print("-----------Tokenize corpus-------------")
     tokenized_sentences = []
     for s in sentences:
         tokens = nltk.word_tokenize(s)
@@ -19,36 +27,53 @@ def train_model():
 
     for s in abc.sents():
         tokenized_sentences.append(s)
+    
+    for s in brown.sents():
+        tokenized_sentences.append(s)
 
-    model= gensim.models.Word2Vec(tokenized_sentences)
-    X= list(model.wv.vocab)
+    print("------------TRAINING FASTTEXT-----------")
+    
+    model = FastText(tokenized_sentences, size=100, window=5, min_count=5, workers=4,sg=1)
 
-    return model, X
+    print("----------------DONE-------------")
+    return model
 
-
-
+def evaluate_replacement():
+    return 0
+    
 def generate_word_pairs(emotion: str, word_pairs: List[Tuple[str, str]]):
     """
     Generates a bunch of word pairs depending on input word pairs and emotion.
     """
 
-    model, vocab = train_model()
+    model = train_model()
 
+    final_pairs = []
     for pair in word_pairs:
+
+        changed_pair =[]
         for word in pair['word_pair']:
-            similar_words = model.similar_by_word(word, 8)
-            print("similar words to " + word , similar_words)
+
+            similar_words_fast = model.wv.similar_by_word(word, 5)
+            print("similar words to " + word , similar_words_fast)
             
+            # now simply picks the closest word (I started a evaluate_replacement method but have not had the time to implement this)
+            changed_pair.append(similar_words_fast[1])
 
+        final_pairs.append(changed_pair)
 
-    return [{'word_pair': (word_pair[0], word_pair[1]), 'verb': 'is'} for word_pair in word_pairs]
+    for p in final_pairs:
+        print(p)
+    
+    # return method needs work such that it returns the correct thing
+    return [{'word_pair': (word_pair[0], word_pair[1]), 'verb': 'is'} for word_pair in final_pairs]
 
 
 # For testing
 if __name__ == '__main__':
     example_emotion = 'sad'
     example_word_pairs = [{'word_pair': ("human", "god"), 'verb': 'was'}, 
-                            {'word_pair': ('animal', 'legged'), 'verb': 'is'}]
+                            ]
     DEBUG = False
     output = generate_word_pairs(example_emotion, example_word_pairs)
     print(output)
