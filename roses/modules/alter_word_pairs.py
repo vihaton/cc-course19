faa8a61db6fecb0bf2989a4361eefea4d9e96600
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2019-05-03 00:56:54 +0300

    some relative path fixes and model download

--- roses/modules/alter_word_pairs.py
+++ roses/modules/alter_word_pairs.py
@@ -1,26 +1,28 @@
 from typing import List, Tuple
 import nltk
-import gensim
+# import gensim
 import os
 from gensim.models import FastText
 from random import randint
 
-# This was a workaround for utils-module not found -error, 
-# there must be a better way to do it.
-import sys
-sys.path.append("..") 
+# # This was a workaround for utils-module not found -error,
+# # there must be a better way to do it.
+# import sys
+# sys.path.append("..")
 
-from utils import read_json_file
+from roses.utils import read_json_file, get_path
 
+# TODO where should these be run?
 nltk.download('abc')
 nltk.download('brown')
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 from nltk.corpus import abc
 from nltk.corpus import brown
 
 
-#TODO 
+# TODO
 # - evaluate similar words and pick a good replacement
 # - make sure the return method returns the correct things - DONE
 # - possibly make a way to save the model and reload it - STARTED, runs nicely here
@@ -29,12 +31,11 @@ from nltk.corpus import brown
 
 # we could possibly save the model to speed up the process - STARTED
 def train_model():
-
-    data = read_json_file("data/bible_kjv_wrangled.json")
+    data = read_json_file("roses/data/bible_kjv_wrangled.json")
     sentences = list(data.values())
     # Do we want everything in lowercase?
     sentences = [s.lower() for s in sentences]
-    
+
     print("-----------Tokenize corpus-------------")
     tokenized_sentences = []
     for s in sentences:
@@ -42,32 +43,35 @@ def train_model():
         tokenized_sentences.append(tokens)
 
     for s in abc.sents():
-        s = list(filter(lambda x:x.isalpha() and len(x)>1, s))
-        s = [x.lower() for x in s] # Do we want everything in lowercase?
+        s = list(filter(lambda x: x.isalpha() and len(x) > 1, s))
+        s = [x.lower() for x in s]  # Do we want everything in lowercase?
         tokenized_sentences.append(s)
-    
+
     for s in brown.sents():
-        s = list(filter(lambda x:x.isalpha() and len(x)>1, s))
-        s = [x.lower() for x in s] # Do we want everything in lowercase?
+        s = list(filter(lambda x: x.isalpha() and len(x) > 1, s))
+        s = [x.lower() for x in s]  # Do we want everything in lowercase?
         tokenized_sentences.append(s)
 
     print("------------TRAINING FASTTEXT-----------")
-    
-    model = FastText(tokenized_sentences, size=100, window=5, min_count=5, workers=4,sg=1)
+
+    model = FastText(tokenized_sentences, size=100, window=5, min_count=5, workers=4, sg=1)
 
     print("----------------DONE-------------")
     return model
 
+
 def evaluate_replacement():
     return 0
-    
+
+
 def generate_word_pairs(emotion: str, word_pairs: List[Tuple[str, str]]):
     """
     Generates a bunch of word pairs depending on input word pairs and emotion.
     """
 
     model_name = 'bible_model'
-    model_dir = '../data/' + model_name
+    model_dir = get_path('data/' + model_name)
+    print(read_json_file("data/bible_kjv_wrangled.json"))
 
     exists = os.path.isfile(model_dir)
     if exists:
@@ -80,8 +84,7 @@ def generate_word_pairs(emotion: str, word_pairs: List[Tuple[str, str]]):
 
     final_pairs = []
     for pair in word_pairs:
-
-        #changed_pair =[]
+        # changed_pair =[]
         noun = find_alternative(pair['word_pair'][0], ['NN', 'NNS', 'NNP', 'NNPS'], model)
         adjective = find_alternative(pair['word_pair'][1], ['JJ', 'JJR', 'JJS'], model)
         changed_pair = [noun, adjective]
@@ -102,23 +105,24 @@ def generate_word_pairs(emotion: str, word_pairs: List[Tuple[str, str]]):
         print(p[1])
         print(type(p[0]))
         print("__________________")
-    
+
     # return method needs work such that it returns the correct thing - DONE
     return [{'word_pair': (word_pair[0], word_pair[1]), 'verb': 'is'} for word_pair in final_pairs]
+
 
 def find_alternative(word, part_of_speechs, model):
     tries = 10
     while True:
         similar_words_fast = model.wv.similar_by_word(word, tries)
-        print("similar words to " + word , similar_words_fast)
+        print("similar words to " + word, similar_words_fast)
 
         tagged = nltk.pos_tag([x[0] for x in similar_words_fast])
         right_part = [x[0] for x in tagged if x[1] in part_of_speechs]
         if right_part:
             index = randint(0, len(right_part))
             return right_part[index]
-        tries += 10 
-    #return 'foo'
+        tries += 10
+        # return 'foo'
 
 
 # For testing
--- roses/utils.py
+++ roses/utils.py
@@ -7,8 +7,14 @@ def read_json_file(filename):
   """
   File name regarding to roses folder.
   """
-  base_path = os.path.dirname(os.path.abspath(__file__))
-  path = "".join([base_path, "/", filename])
+  path = get_path(filename)
   with open(path) as f:
     content = json.loads(f.read())
   return content
+
+
+def get_path(filename):
+  base_path = os.path.dirname(os.path.abspath(__file__))
+  return "".join([base_path, "/", filename])
+
+
